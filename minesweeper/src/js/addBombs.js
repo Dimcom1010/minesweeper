2f7491525ba7e150f771519e42849b1251e21368
Dimcom1010 <dimcom1010@gmail.com>
2023-05-13 19:56:43 +0300

    feath: add function add bombs

---
+++ minesweeper/src/js/addBombs.js
@@ -0,0 +1,17 @@
+import { getRandom } from "./random.js"
+export function addBombs(bombs, matrix) {
+    let iteration = bombs;
+    const matrixW = matrix.length;
+    const matrixH = matrix[0].length;
+    while (iteration) {
+        const x = getRandom(matrixW - 1);
+        const y = getRandom(matrixH - 1);
+        const selectMatrixPoint=matrix[x][y]
+        if(!selectMatrixPoint){
+            matrix[x][y]=1;
+            iteration--;
+        }
+    }
+    return matrix
+}
+// Test complite
