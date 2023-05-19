c87d34712d15bbf07bed3c156c8b5d6a90c71ce2
Dimcom1010 <dimcom1010@gmail.com>
2023-05-19 21:07:12 +0300

    featch: add audio min and vios

--- minesweeper/src/js/audio.js
+++ minesweeper/src/js/audio.js
@@ -5,11 +5,14 @@ export function audioClick(){
     const audio = 'src/audio/click.mp3';
     !variables.isAudio && playAudio(audio)
 }
-export function audio(){
+export function audioBoom(){
     const audio = 'src/audio/boom.mp3';
     !variables.isAudio && playAudio(audio)
 }
-
+export function audioWin(){
+    const audio = 'src/audio/syn.mp3';
+    !variables.isAudio && playAudio(audio)
+}
 
  function playAudio(audio) {
     variables.isAudio=true
